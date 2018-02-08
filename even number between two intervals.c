6af9fae607764c9594bf27d9ed06a69f6a03bca0
dkdhineshs <35652666+dkdhineshs@users.noreply.github.com>
2018-02-08 16:16:10 +0530

    Create even number between two intervals.c

---
+++ even number between two intervals.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+
+int main(){
+
+           int a,b,rem,i;
+
+           printf("   Give the First number for the Range :   \n");
+
+           scanf("%d",&a);
+
+           printf("   Give the Final number for the Range :   \n");
+
+           scanf("%d",&b);
+
+           printf("\n The Even numbers between %d and %d are ",a,b); 
+
+           for(i=a; i<=b; ++i){
+
+               rem = i % 2;
+
+               if(rem == 0)
+
+               printf("\n  %d",i);
+
+           }
+
+    return 0; 
+
+    } 

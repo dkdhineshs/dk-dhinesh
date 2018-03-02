b012808e5897751cf44d55e47ff97dd6523e7183
dkdhineshs <35652666+dkdhineshs@users.noreply.github.com>
2018-03-02 14:57:47 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a ;
+    printf("whether saturday it or monday yes or no");
+    scanf("%c",&a);
+    if(a=='y')
+    {
+    printf(" that day is holiday");
+    }
+    else if(a=='n')
+    {
+    printf("that day is holiday");
+}
+
+}
